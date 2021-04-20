7ab45adeeefc6ff84bef5fd91271d199414b1fb5
apsampaio <andre03.sampaio@gmail.com>
2021-04-20 18:24:12 -0300

    add neighbors and visited cells

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import p5 from "p5";
 
 export const cellSize = 40;
 export const canvasSize = 400;
-const cols = Math.floor(canvasSize / 40);
-const rows = Math.floor(canvasSize / 40);
-const grid: Cell[] = [];
+export const cols = Math.floor(canvasSize / 40);
+export const rows = Math.floor(canvasSize / 40);
+export const grid: Cell[] = [];
+let currentCell: Cell;
 
 const sketch = (s: p5) => {
   s.setup = () => {
@@ -16,12 +17,21 @@ const sketch = (s: p5) => {
         grid.push(cell);
       }
     }
+    s.frameRate(5);
+    currentCell = grid[0];
   };
 
   s.draw = () => {
     s.background(51);
     for (let i = 0; i < grid.length; i++) {
       grid[i].show(s);
+    }
+
+    currentCell.visited = true;
+    const next = currentCell.checkNeighbors();
+    if (next) {
+      next.visited = true;
+      currentCell = next;
     }
   };
 };
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,16 +1,25 @@
 import p5 from "p5";
-import { cellSize } from "../index";
+import { cellSize, cols, rows, grid } from "../index";
 
 export default class Cell {
-  i: number;
-  j: number;
-  walls: Boolean[];
+  private i: number;
+  private j: number;
+  private walls: Boolean[] = [];
+  public visited = false;
 
   constructor(i: number, j: number) {
     this.i = i;
     this.j = j;
-    this.walls = [];
   }
+
+  public index = (i: number, j: number) => {
+    // Deny edge cases
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+      return -1;
+    }
+
+    return i + j * cols;
+  };
 
   public show = (s: p5) => {
     const w = cellSize;
@@ -24,7 +33,29 @@ export default class Cell {
     this.walls[2] && s.line(x + w, y + w, x, y + w); // BOTTOM
     this.walls[3] && s.line(x, y + w, x, y); // LEFT
 
-    // s.noFill();
-    // s.rect(x, y, cellSize, cellSize);
+    if (this.visited) {
+      s.fill("red");
+      s.rect(x, y, cellSize, cellSize);
+    }
+  };
+
+  public checkNeighbors = () => {
+    const neighbors: Cell[] = [];
+    const top = grid[this.index(this.i, this.j - 1)];
+    const right = grid[this.index(this.i + 1, this.j)];
+    const bottom = grid[this.index(this.i, this.j + 1)];
+    const left = grid[this.index(this.i - 1, this.j)];
+
+    top && !top.visited && neighbors.push(top);
+    right && !right.visited && neighbors.push(right);
+    bottom && !bottom.visited && neighbors.push(bottom);
+    left && !left.visited && neighbors.push(left);
+
+    if (neighbors.length) {
+      const random = Math.floor(Math.random() * neighbors.length);
+      return neighbors[random];
+    } else {
+      return undefined;
+    }
   };
 }

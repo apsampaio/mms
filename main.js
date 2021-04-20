cbbf2edc9197fc36e8edc0bf51bb3e6314fcb146
apsampaio <andre03.sampaio@gmail.com>
2021-04-19 22:24:38 -0300

    add electron to preview

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    alwaysOnTop: true,
+    title: "Micromouse Simulator",
+    width: 600,
+    height: 400,
+    minWidth: 600,
+    minHeight: 400,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL("http://localhost:1234/");
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
